502e2bb52619e064850748c0c4f028ff05610758
ilyasozkayaa <appinventer.ilyas@gmail.com>
2019-01-24 20:35:31 +0300

    Promosyon adlı class içideki metotlar dolduruldu.

--- BLL/Model/PromosyonPrimIslemleri.cs
+++ BLL/Model/PromosyonPrimIslemleri.cs
@@ -9,19 +9,57 @@ namespace BLL.Model
 {
     public class PromosyonPrimIslemleri : IPromosyonPrimIslemleri
     {
+        CRMContext ent = new CRMContext();
         public bool promosyonEkle(Promosyon p)
         {
-            throw new NotImplementedException();
+            bool sonuc = false;
+            try
+            {
+                ent.Promosyons.Add(p);
+                ent.SaveChanges();
+                sonuc = true;
+            }
+            catch (Exception ex)
+            {
+
+                string message = ex.Message;
+            }
+            return sonuc;
         }
 
         public bool promosyonGuncelle(Promosyon p)
         {
-            throw new NotImplementedException();
+            bool sonuc = false;
+            Promosyon promosyon = (from pr in ent.Promosyons where pr.Id == p.Id select pr).FirstOrDefault();
+            try
+            {
+                promosyon.PromosyonAdi = p.PromosyonAdi;
+                promosyon.PromosyonOrani = p.PromosyonOrani;
+
+                sonuc = true;
+            }
+            catch (Exception ex)
+            {
+
+                string message = ex.Message;
+            }
+            return sonuc;
         }
 
         public bool promosyonSil(int ID)
         {
-            throw new NotImplementedException();
+            bool sonuc = true;
+            Promosyon SilinecekPromosyon = (from p in ent.Promosyons where p.Id == ID select p).FirstOrDefault();
+            try
+            {
+                SilinecekPromosyon.Silindi = true;
+            }
+            catch (Exception ex)
+            {
+
+                string message = ex.Message;
+            }
+            return sonuc;
         }
     }
 }
