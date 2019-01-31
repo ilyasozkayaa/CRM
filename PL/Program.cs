eb8a52f4883c8cc3b8b9f441e57a90224b4ae66f
büşra aydoğan <busra.aydogan14@outlook.com>
2019-02-01 00:47:31 +0300

    urunıslemlerı

--- PL/Program.cs
+++ PL/Program.cs
@@ -22,7 +22,7 @@ namespace PL
             }
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new  frmAnasayfa());
+            Application.Run(new  frmGiris());
         }
     }
 }
